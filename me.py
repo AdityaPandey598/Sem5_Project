dc36d3b77020b95c5b1c6938113ae69f8f9c2dfd
Aditya <aditypnd4@gmail.com>
2025-10-30 22:50:47 +0530

    Corrections

--- me.py
+++ me.py
@@ -5,6 +5,13 @@ import plotly.express as px
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import MinMaxScaler
 
+def data_report(file_name):
+    fpath='/home/adityapandey/Downloads/project-sem5/Data Sets/'
+    from ydata_profiling import ProfileReport
+    df = pd.read_csv(fpath+file_name)
+    profile = ProfileReport(df, title="final report1", explorative=True)
+    profile.to_file("final report1.html")
+
 def load_data():
     fpath='/home/adityapandey/Downloads/project-sem5/Data Sets/'
     indian_students = pd.read_csv(fpath+"IndianStudentsAbroad.csv")
@@ -15,41 +22,18 @@ def load_data():
 
 indian_students, cost_living, tuition, reputation = load_data()
 indian_students.drop(columns=['index'], inplace=True)
-print(indian_students.head())
-
-order = indian_students.sort_values('No of Indian Students')['Country'].unique().tolist()
-encoder = OrdinalEncoder(categories=[order])
-indian_students['Country_Encoded'] = encoder.fit_transform(indian_students[['Country']])
-
-num_cols = ['Country_Encoded','No of Indian Students','Percentage','Unnamed: 3']
-scaler = MinMaxScaler()
-indian_students[num_cols] = scaler.fit_transform(indian_students[num_cols])
-indian_students.drop(columns=['Unnamed: 3'], inplace=True)
-print(indian_students)
-
+indian_students.loc[indian_students['Country']=='US(2015-16)','Country']='United States'
+indian_students.loc[indian_students['Country']=='UK(2015-16)','Country']='United Kingdom'
 tuition.columns = tuition.columns.str.strip().str.lower().str.replace(" ", "_")
-
+tuition.loc[tuition['country']=='USA','country']='United States'
+tuition.loc[tuition['country']=='UK','country']='United Kingdom'
+print(tuition.head())
 tuition["total_cost"] = (tuition["tuition_usd"] * tuition["duration_years"]) + \
                    (tuition["rent_usd"] *tuition["duration_years"]) + \
                 tuition["visa_fee_usd"] +tuition["insurance_usd"]
-nums_col2=['exchange_rate','living_cost_index','tuition_usd','duration_years','rent_usd','visa_fee_usd','insurance_usd','total_cost']  
-tuition[nums_col2]=scaler.fit_transform(tuition[nums_col2]) 
-print(tuition.head())
-
 cost_living.columns = cost_living.columns.str.strip().str.lower().str.replace(" ", "_")
-nums_col3=['cost_of_living_index','rent_index','cost_of_living_plus_rent_index','groceries_index','restaurant_price_index','local_purchasing_power_index']
-cost_living[nums_col3]=scaler.fit_transform(cost_living[nums_col3]) 
 print(cost_living.head())
-
-
 reputation.columns = reputation.columns.str.strip()
-nums_col4=['RANK_2024','Region','SIZE','FOCUS','RES.','STATUS','Academic_Reputation_Score', 'Academic_Reputation_Rank', 'Employer_Reputation_Score', 'Employer_Reputation_Rank', 'Faculty_Student_Score', 'Faculty_Student_Rank', 'Citations_per_Faculty_Score', 'Citations_per_Faculty_Rank', 'International_Faculty_Score', 'International_Faculty_Rank', 'International_Students_Score', 'International_Students_Rank', 'International_Research_Network_Score', 'International_Research_Network_Rank', 'Employment_Outcomes_Score', 'Employment_Outcomes_Rank', 'Sustainability_Score', 'Sustainability_Rank']
-reputation.drop(columns=nums_col4, inplace=True)
-nums_col5=['Overall_Score']
-# Ensure numeric dtype, then scale
-reputation[nums_col5] = scaler.fit_transform(
-    reputation[nums_col5].apply(pd.to_numeric, errors='coerce')
-)
 print(reputation.head())
 print(reputation.columns.tolist())
 
@@ -60,7 +44,12 @@ df = tuition.merge(cost_living, on="country", how="left") \
             .merge(reputation, left_on="university", right_on="institution_name", how="left")
 # Example: Drop rows where 'rank' is NaN (missing)
 df_cleaned = df.dropna(subset=['rank_2025'])
-
+df_cleaned = df_cleaned.drop(df_cleaned.loc[df_cleaned['country'] == 'UAE'].index)
+df_cleaned = df_cleaned.drop(df_cleaned.loc[df_cleaned['country'] == 'South Korea'].index)
+df_cleaned=df_cleaned.drop(columns=['institution_name','location'])
 print(df_cleaned.head())
-df_cleaned.to_csv('/home/adityapandey/Downloads/project-sem5/Data Sets/merged_indian_students_abroad.csv', index=False)
+df_cleaned=df_cleaned.drop(columns=['unnamed:_3','rank_2024','region','size','focus','res.','status'])
+fpath='/home/adityapandey/Downloads/project-sem5/Data Sets/'
+df_cleaned.to_csv(fpath+"merged_indian_students_abroad.csv", index=False)
+
 
